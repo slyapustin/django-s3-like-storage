6a4c81a3bd6ec90699a281e4bc9231f74e4ce3e8
Sergey Lyapustin <s.lyapustin@gmail.com>
2019-07-30 12:17:34 +0200

    Set Django Debug mode via environment variables

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*',]
 
